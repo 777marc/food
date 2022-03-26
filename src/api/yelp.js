cdc68e5093afa8a5aecac69f3bf8517a2b54fee7
Marc Mendoza <777marc@gmail.com>
2022-03-26 14:25:13 -0400

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer d1bvjDLQPeIW7lGtwy0LcrfCki4fgl9b90wH4yRvwGKa8WQ4kwLXVLo0vQQ-jAkNRAMgYTvFgU4r22KZzy8lvnh4wkt4mKSxvfmq3KkVo5TpNcUEGxAJZZkEXRY-YnYx",
+      "Bearer <yelp api token>,
   },
 });
