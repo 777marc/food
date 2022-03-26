4c54fe4976b0e5170e7050f6cb126c82ea4a48fb
Marc Mendoza <777marc@gmail.com>
2022-03-26 10:58:01 -0400

    api wiring

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange, onTermSubmitted }) => {
+  return (
+    <View style={styles.background}>
+      <Ionicons style={styles.icon} name="ios-search" color="black" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmitted}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    marginTop: 10,
+    flexDirection: "row",
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 10,
+  },
+});
+
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errors, setErrors] = useState("");
+
+  const searcAPI = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          term: searchTerm,
+          location: "orlando, fl",
+          limit: 25,
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrors("there was a problem completing your request");
+    }
+  };
+
+  // iniitial load
+  useEffect(() => {
+    searcAPI("pasta");
+  }, []);
+
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmitted={searcAPI}
+      />
+      <Text style={styles.text}>we have {results.length} results</Text>
+      {errors ? <Text>{errors}</Text> : null}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#fff",
+  },
+  text: {
+    padding: 15,
+    color: "blue",
+  },
+});
 
 export default SearchScreen;
